b7893322747b20208aaf80e443895d5dd907f0e2
Artem <dzibronidzibroniii@gmail.com>
2026-03-28 00:34:21 +0200

    15

--- docs/JS/Prak.js
+++ docs/JS/Prak.js
@@ -1,15 +1,21 @@
 "use strict";
-{    
+
+{
     let name = prompt("Ваше ім'я?");
-    let plece = prompt("Ваше місто?");
+    let place = prompt("Ваше місто?");
     let like_color = prompt("Ваш улюблений колір?");
     let doyou_work = prompt("Ви працюєте?");
-    let i = prompt("Ваш вік?");
-        if (i < 18) {
-            alert("Ви не досягли повноліття, доступ заборонено!");
-        }
-        Number(i);
-        
-    alert("Результат:"+name+"-"+"Місто:"+plece+"-"+"Колір:"+like_color+"-"+"Працюєте:"+doyou_work+"-"+"Вік:"+i);
-    
+    let i = Number(prompt("Ваш вік?")); // одразу перетворюємо
+
+    if (i < 18) {
+        alert("Ви не досягли повноліття, доступ заборонено!");
+    } else {
+        alert(
+            "Результат: " + name +
+            " - Місто: " + place +
+            " - Колір: " + like_color +
+            " - Працюєте: " + doyou_work +
+            " - Вік: " + i
+        );
+    }
 }
